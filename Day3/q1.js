09243339c31b4d2d59e9a24342b7d61d2f261d69
SRIJANNA DAS <75168944+srijannadas@users.noreply.github.com>
2022-09-13 01:35:04 +0530

    day 3

---
+++ Day3/q1.js
@@ -0,0 +1,17 @@
+/* 
+You are given three variables: name, age and isEmployeed
+add your name as string datatype in variable name
+add your age in variable age as number (must be greater than 0)
+add your employment status as a boolean in isEmployeed 
+*/
+
+var name, age, isEmployeed
+
+name = "Srijanna";
+age = 20;
+isEmployeed = false;
+/* start code here: */
+
+
+/* end here */
+try { module.exports = { name, age, isEmployeed } } catch(e) {}
